780d05b953d77dcc79c772e5656e2363ea6d15ff
Mark Whang <hwhang0917@gmail.com>
2019-10-17 18:29:55 -0700

    commit hw5

--- HW_5/Assignment_5.md
+++ HW_5/Assignment_5.md
@@ -4,10 +4,48 @@ CSCI 440: Artificial Intelligence
 
 Assignment #4
 
-10/UNKNOWN/2019
+10/23/2019
 
 1. **(5 points)** You get full points if either
    * all the rest of the assignment elements work or ...
    * you spend a considerable amount of time working on trying to get the assignment working
      * Record the approximate amount of time working on the assignment
      * Detail the parts you struggled with and your attempts to get them working
+
+2. Implement the minmax and alpha beta algorithms to play TicTacToe. Consider the TicTacToe board. We can label each position 0 to 8 as shown below.
+
+   |   0   |   1   |   2   |
+   | :---: | :---: | :---: |
+   | **3** | **4** | **5** |
+   | **6** | **7** | **8** |
+
+   Then the state of the board is given by a string S, where S[i]='x' or 'X' if the first player has made that move and, S[i] = 'o' or 'O' if the second player has made that move, and S[i] = ' ' or '.' otherwise. Player 1(x) is the maximizing player and Player 2(o) is the minimizing one.
+
+   * a) **(1 point)** Implement a function that given a state of the board will return
+
+     * 1 if it is a wining position for player 1(x)
+     * -1 if it is a winning position for player 2(o)
+     * 0 if the position is not a winning position for either side yet
+
+     ```C++
+     int getAttackScore(int Q[]);
+     ```
+
+     
+
+   * b) **(2 points)** The minmax function has already implemented, but in order for it to work, you need to implement the minvalue and maxvalue functions (pseudocode in the textbook page 166).
+
+     ```C++
+     double minvalue(string state);
+     double maxvalue(string state);
+     ```
+
+     
+
+   * c) **(2 points)** Implement the alpha-beta algorithm (pseudocode in the textbook page 170). 
+
+     ```C++
+     int minmaxAB(string state, bool xturn);
+     ```
+
+     
---
+++ HW_5/tictactoe_StudentTest.cpp
@@ -0,0 +1,415 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
+
+
+void drawBoard(string state);
+
+string play(string state, bool xturn);
+string playAI(string state, bool xturn);
+string playAIab(string state, bool xturn);
+
+
+int minimax(string state, bool xturn);
+double minvalue(string state);
+double maxvalue(string state);
+
+int minimaxAB(string state, bool xturn);
+double minvalueAB(string state, double alpha, double beta);
+double maxvalueAB(string state, double alpha, double beta);
+
+int eval(string state);  //given a state s, return 1 if x wins, -1 if o wins, 0  undetermined (may or may not be a leaf)
+
+
+
+int main()
+{
+	cout << "Testing eval function ...\n";
+	system("pause");
+
+	string s[] = { ".........", "o..xooxXx","xoxxoxoO.", "x.oxOxo..","......xox","xxooxxoxo",".x..o...x" };
+
+	for (int i = 0; i < 7; i++)
+	{
+		system("cls");
+		drawBoard(s[i]);
+		cout << "Evaluation : " << eval(s[i]) << endl;
+		system("pause");
+	}
+
+
+	cout << "Testing minimax by game playing ...\n";
+	system("pause");
+	string state;
+	bool xturn;
+
+	//AI goes first
+	state = ".........";
+	drawBoard(state);
+	xturn = true;
+	for (int i = 0; i < 9; i++)
+	{
+		if (xturn)
+			state = playAI(state, xturn);
+		else
+			state = play(state, xturn);
+		if (eval(state) > 0)
+		{
+			cout << "AI (x) wins !" << endl;
+			break;
+		}
+		if (eval(state) < 0)
+		{
+			cout << "Player 2 (o) wins !" << endl;
+			break;
+		}
+		if (eval(state) == 0 && i == 8)
+		{
+			cout << "Draw!" << endl;
+			break;
+		}
+		xturn = !xturn;
+	}
+
+	system("pause");
+	xturn = true;
+	state = ".........";
+
+	//AI goes second
+	for (int i = 0; i < 9; i++)
+	{
+		if (xturn)
+			state = play(state, xturn);
+		else
+			state = playAI(state, xturn);
+		if (eval(state) < 0)
+		{
+			cout << "AI (o) wins !" << endl;
+			break;
+		}
+		if (eval(state) > 0)
+		{
+			cout << "Palyer 1 (x) wins !" << endl;
+			break;
+		}
+		if (eval(state) == 0 && i == 8)
+		{
+			cout << "Draw!" << endl;
+			break;
+		}
+		xturn = !xturn;
+	}
+	system("pause");
+
+
+	cout << "Testing minimax alpha-beta by game playing ...\n";
+	system("pause");
+
+
+	//AI goes first
+	state = ".........";
+	drawBoard(state);
+	xturn = true;
+	for (int i = 0; i < 9; i++)
+	{
+		if (xturn)
+			state = playAIab(state, xturn);
+		else
+			state = play(state, xturn);
+		if (eval(state) > 0)
+		{
+			cout << "AI (x) wins !" << endl;
+			break;
+		}
+		if (eval(state) < 0)
+		{
+			cout << "Player 2 (o) wins !" << endl;
+			break;
+		}
+		if (eval(state) == 0 && i == 8)
+		{
+			cout << "Draw!" << endl;
+			break;
+		}
+		xturn = !xturn;
+	}
+
+	system("pause");
+	xturn = true;
+	state = ".........";
+
+	//AI goes second
+	for (int i = 0; i < 9; i++)
+	{
+		if (xturn)
+			state = play(state, xturn);
+		else
+			state = playAIab(state, xturn);
+		if (eval(state) < 0)
+		{
+			cout << "AI (o) wins !" << endl;
+			break;
+		}
+		if (eval(state) > 0)
+		{
+			cout << "Palyer 1 (x) wins !" << endl;
+			break;
+		}
+		if (eval(state) == 0 && i == 8)
+		{
+			cout << "Draw!" << endl;
+			break;
+		}
+		xturn = !xturn;
+	}
+	system("pause");
+
+	return 0;
+}
+
+
+/********************************************************/
+///Drawing and game play functions////////////////////////
+void drawBoard(string state)
+{
+	string board[] = { 
+	".....|.....|.....",
+	".....|.....|.....",
+	".....|.....|.....",
+	"-----+-----+-----",
+	".....|.....|.....",
+	".....|.....|.....",
+	".....|.....|.....",
+	"-----+-----+-----",
+	".....|.....|.....",
+	".....|.....|.....",
+	".....|.....|.....", };
+	cout << endl;
+
+	for (int i = 0; i < 9; i++)
+	{
+		int a = i / 3, b = i % 3;
+		board[a * 4 + 1][b * 6 + 2] = state[i];
+	}
+
+	cout << "      0     1     2" << endl;
+	cout << endl;
+
+	for (int i = 0; i < 11; i++)
+	{
+		if (i % 4 == 1)
+			cout << i / 4 << "   ";
+		else
+			cout << "    ";
+		for (int j = 0; j < 17; j++)
+		{
+			char ch = toupper(board[i][j]);
+			switch (ch)
+			{
+			case '.': cout << ' '; break;
+			case '|': cout << char(186); break;
+			case '-': cout << char(205); break;
+			case '+': cout << char(206); break;
+			case 'X': cout << 'X'; break;
+			case 'O': cout << 'O'; break;
+			}
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+string play(string state, bool xturn)
+{
+	int r, c;
+	system("cls");
+	drawBoard(state);
+	if (xturn)
+	{
+		cout << "X-player, enter the coordinates of where you want to play (row then column):";
+		cin >> r >> c;
+		int ind = r * 3 + c;
+		while (r > 2 || r < 0 || c>2 || c < 0 || state[ind] == 'x' || state[ind] == 'X' || state[ind] == 'o' || state[ind] == 'O')
+		{
+			cout << "Illegal move.  Try again : ";
+			cin >> r >> c;
+			ind = r * 3 + c;
+		}
+		state[ind] = 'x';
+	}
+	else
+	{
+		cout << "O-player, enter the coordinates of where you want to play (row then column):";
+		cin >> r >> c;
+		int ind = r * 3 + c;
+		while (r > 2 || r < 0 || c>2 || c < 0 || state[ind] == 'x' || state[ind] == 'X' || state[ind] == 'o' || state[ind] == 'O')
+		{
+			cout << "Illegal move.  Try again : ";
+			cin >> r >> c;
+			ind = r * 3 + c;
+		}
+		state[ind] = 'o';
+	}
+	system("cls");
+	drawBoard(state);
+	return state;
+}
+
+string playAI(string state, bool xturn)
+{
+	int action = minimax(state, xturn);// assign action based on algorithm
+
+	if (xturn)
+	{
+		state[action] = 'x';
+	}
+	else
+	{
+		state[action] = 'o';
+	}
+	system("cls");
+	drawBoard(state);
+	cout << "AI played : (" << action / 3 << ", " << action % 3 << ")\n";
+	system("pause");
+	return state;
+}
+
+string playAIab(string state, bool xturn)
+{
+	int action = minimaxAB(state, xturn);// assign action based on algorithm
+
+	if (xturn)
+	{
+		state[action] = 'x';
+	}
+	else
+	{
+		state[action] = 'o';
+	}
+	system("cls");
+	drawBoard(state);
+	cout << "AI played : (" << action / 3 << ", " << action % 3 << ")\n";
+	system("pause");
+	return state;
+}
+
+/*********************************************************/
+
+int minimax(string state, bool xturn)
+{
+	int action = 0;
+	for (int i = 0; i < 9; i++)
+		state[i] = tolower(state[i]);
+
+	if (xturn)  //max players turn
+	{
+		//pick the action that maximizes 
+		double maxv = -2000;
+		action = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			if (state[i] != 'x' && state[i] != 'o')  //a valid move
+			{
+				string newstate = state;
+				newstate[i] = 'x';
+				double val = minvalue(newstate);
+				if (val > maxv)
+				{
+					maxv = val;
+					action = i;
+				}
+			}
+		}
+	}
+	else
+	{
+		//pick the action that minimizes 
+		double minv = 2000;
+		action = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			if (state[i] != 'x' && state[i] != 'o')  //a valid move
+			{
+				string newstate = state;
+				newstate[i] = 'o';
+				double val = maxvalue(newstate);
+				if (val < minv)
+				{
+					minv = val;
+					action = i;
+				}
+			}
+		}
+	}
+	return action;
+}
+
+/*********************************************************/
+
+int eval(string s)  //given a state s, return 1 if x wins, -1 if o wins, 0  undetermined (may or may not be a leaf)
+{
+	// 2D array of winning index combinations
+	// Note: there are total of 8 ways to win 3x3 tictactoe
+	int winning_index[8][3] = {
+		{0,1,2}, {3,4,5}, {6,7,8}, // Row
+		{0,3,6}, {1,4,7}, {2,5,8}, // Column
+		{0,4,9}, {2,4,6} // Diagonal
+	};
+
+	for (int i = 0; i < 8; i ++) { // Loop through array of combinations
+		int x_Count = 0, o_Count = 0; // Counter for number of Xs and Os
+
+		for (int j = 0; j < 3; j ++) { // Loop through individual combination
+			switch(s[winning_index[i][j]]) {
+				case 'x':
+				case 'X':
+					x_Count++;
+					break;
+				case 'o':
+				case 'O':
+					o_Count++;
+					break;
+				default:
+					break;
+			}
+		}
+
+		// Check for winner
+		if (x_Count == 3) 
+			return 1;
+		else if (o_Count == 3)
+			return -1;
+	}
+
+	// No winner
+	return 0;
+}
+
+double minvalue(string state)
+{
+	return 0;
+}
+
+double maxvalue(string state)
+{
+	return 0;
+}
+
+
+int minimaxAB(string state, bool xturn)
+{
+	return 0;
+}
+double minvalueAB(string state, double alpha, double beta)
+{
+	return 0;
+}
+double maxvalueAB(string state, double alpha, double beta)
+{
+	return 0;
+}
