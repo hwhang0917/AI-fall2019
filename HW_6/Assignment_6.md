40c80dba4d1a2d62c5f3f8f8293805ef79406d8e
Mark Whang <hwhang0917@gmail.com>
2019-10-24 15:47:32 -0700

    Begin homework 6

--- HW_6/Assignment_6.md
+++ HW_6/Assignment_6.md
@@ -7,7 +7,61 @@ Assignment #4
 10/23/2019
 
 1. **(5 points)** You get full points if either
+   
    * all the rest of the assignment elements work or ...
    * you spend a considerable amount of time working on trying to get the assignment working
      * Record the approximate amount of time working on the assignment
-     * Detail the parts you struggled with and your attempts to get them working
+  * Detail the parts you struggled with and your attempts to get them working
+
+2. Sudoku can be viewed as CSP(Constraints Satisfaction Problem).
+   * The **variables** are *the individual cells, numbers 0 to 80,* based on the location on the board. A cell located in row A (can be 0 to 8) and column B (can be 0 to 8), will have index 9*A+B.
+   
+   * The **constraints** are converted to binary constraints and given by the constraints graph, represented by a *boolean matrix C\[81]\[81]* where C\[X]\[Y]=true if and only if cells x and y must be different (i.e. x and y are in the same row, same column, or same 3x3 sub-grid).
+   
+   * The **domain** for each of the variables are the numbers 1 to 9 and represented by *an array of vectors, vector<int> domain\[81].* Each individual Sudoku puzzle comes with a predefined assignment of values for particular cells (unary constraints). We reflect these unary constraints immediately on the domain. For example, if cell (variable) indexed by 70 is defined by puzzle to be 4, then domain\[70]={4} (instead of {1,2,3,4,5,6,7,8,9}).
+   
+   * An **assignment** is the assigning of specific value to each cell (variable) and is represented by *a vector of integers, vector<int> assignment*.
+   
+   * Furthermore we define a structure for each binary constraint which is an arc in the constraint graph.
+   
+     ```C++
+     struct arcType {
+     	int source;
+     	int destination;
+     };
+     ```
+   
+     So the *constraint that xi and xj must be different can be represented by arcType c={xi, xj}*. This representation will be useful when implementing the AC-3 algorithm.
+     
+   
+   a) (**1 point**) Implement the revise function with pseudocode shown in Figure 6.3, page 209.
+   
+   ```C++
+   bool revise(int xi, int xj, vector<int> domain[81], const bool C[81][81]);
+   ```
+   
+   b) (**1 point**) Implement the arc-consistency algorithm AC-3 with pseudocode shown in Figure 6.3, page 209. A suggestion is to make the queue a queue of *arcType*, and initialize it by all the arcs {xi, xj} that satisfy *C\[xi]\[xj]==true*.
+   
+   ```C++
+   bool AC3(vector<int> domain[81], const bool C[81][81]);
+   ```
+   
+   c) (**1 point**) A basic implementation of the backtrack algorithm on Figure 6.5, page 215 is given to you (called *backtrackCSP*). It does not do any inference r use any heuristics. Implement the backtrack algorithm with the MRV heuristics built in. You can build the function from scratch, or you can make a simple modification the function provided. The variable count keeps track of how many variables have already been assigned.
+   
+   ```C++
+   vector<int> backtrackCSP_MRV(vector<int> assignment, int count, vector<int> domain[81], const bool C[81][81]);
+   ```
+   
+   d) (**2 points**) You are given a sets of puzzle in the main function.
+   
+   * Run the backtrackCSP and backtrackCSP_MRV algorithms on the puzzles. Record the average time to solve them.
+   * Apply AC3 to each puzzle, then run backtrackCSP and backtrackCSP_MRV algorithms on them. Record the average time to solve them.
+   
+   Record the results in the table below:
+   
+   |                       | Backtrack | Backtrack+MRV | Backtrack+AC3 | Backtrack+AC3+MRV |
+   | --------------------- | --------- | ------------- | ------------- | ----------------- |
+   | Average Solution time |           |               |               |                   |
+   
+   
+
---
+++ HW_6/sudokuStudentTester.cpp
@@ -0,0 +1,424 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>    // std::random_shuffle
+#include <vector>       // std::vector
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
+#include <chrono>
+#include <queue>
+
+
+using namespace std;
+using namespace std::chrono;
+
+///////////////////////////////////////////////////////
+//functions for drawing the board//////////////////////
+void getSudokuBoard(string table[]);
+void displayBoard(string table[]);
+void fillDisplayBoard(string board[], int sudokuM[][9]);
+void fillDisplayBoard(string board[], vector<int> assignment);
+void displayBoard(string table[], int T[][9]);
+void displaySudokuCSP(vector<int> assignment, vector<int> domain[81]);
+void displaySudokuSimple(vector<int> assignment);
+/////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
+///Helper functions and structures///////////////////////
+void fillConst(bool C[81][81]);   //creates the constraint graph
+void getSudokuCSP(string puzzle, vector<int>& assignment, vector<int> domain[81]);
+//given a puzzle as a string of 81 characters 0-9 
+//return the apropriate assigment and domain
+
+struct arcType   //a structure for a binary constraint, i.e., an edge in the graph
+{
+	int source;
+	int destination;
+};
+
+
+
+
+vector<int> backtrackCSP(vector<int> assignment, int count, vector<int> domain[81], const bool C[81][81]);
+vector<int> backtrackCSP_MRV(vector<int> assignment, int count, vector<int> domain[81], const bool C[81][81]);
+
+bool AC3(vector<int> domain[81], const bool C[81][81]);
+bool revise(int xi, int xj, vector<int> domain[81], const bool C[81][81]);
+
+
+int main()
+{
+
+	string puzzles[] = { "000500002700600800001000300076050000002000000080000700390072600020000090050018030",
+		"800201040000000107709006003006340000003060000002000008020903000000080090000007080",
+		"100003400000600020008005300001004070406800100000000005065080002802001600000000000",
+		"809402037020090500360000800500000000080037000076008000000000093700300000900054000",
+		"050800607000600520100509080040001700007006490000000000921000070000005100560000000",
+		"001200000000047000789006000060008701070002400100000906036000000800090000400070000",
+		"400070009008605070005000026000800704670000500000000000069000000000590010020010040",
+		"080006070002407010003000000000010800201060037300000240106000000034070000000090000",
+		"000000040000000906350100800510030400004910008060004000030000160200000059100000000",
+		"000000007000050060057000824000005782090000600008070000400009050015068200009000408",
+		"000639710000000340900000000603700008000081000000500000005370600009040800700000100",
+		"000000030000004060000090400002006000106300500097500004001005900049200007070010040",
+		"000000003029003500800900006600000190500009000001600057000300000080000020176200300",
+		"300000000901000000004701000080020073003050092000008001490500000100300007008000109",
+		"012060000900200860005400120670004000401000090000820040096100000000090000004000700",
+		"400000805030000000000700000020000060000080400000010000000603070500200000104000000",
+		"520006000000000701300000000000400800600000050000000000041800000000030020008700000",
+		"600000803040700000000000000000504070300200000106000000020000050000080600000010000",
+		"480300000000000071020000000705000060000200800000000000001076000300000400000050000",
+		"000014000030000200070000000000900030601000000000000080200000104000050600000708000" };
+
+	bool C[81][81];
+	vector<int> assignment;
+	vector <int> domain[81];
+	string table[20];  //for display purposes
+	fillConst(C);
+	getSudokuCSP(puzzles[0], assignment, domain);
+	getSudokuBoard(table);
+	fillDisplayBoard(table, assignment);
+	cout << "The puzzle:\n";
+	displayBoard(table);
+	cout << endl << "The CSP domain:\n";
+	displaySudokuCSP(assignment, domain);
+
+	cout << "Testing the revise function...\n";
+	for (int i = 0; i < 3; i++)
+	{
+		int xi, xj;
+		cout << "Enter the two cells you want to resolve (indexed 0-80 as discussed) : ";
+		cin >> xi >> xj;
+		if (revise(xi, xj, domain, C))
+		{
+			cout << "The domain of cell #" << xi << " has been revised.  Here is the new domain: \n{";
+			for (auto v : domain[xi])
+				cout << v << ", ";
+			cout << "}.\n";
+		}
+		else
+			cout << "No revisions.\n";
+	}
+	system("pause");
+	system("cls");
+	cout << "The status of the overall domain after the revisions is :\n";
+	cout << endl << "The CSP domain:\n";
+	displaySudokuCSP(assignment, domain);
+	system("pause");
+
+
+	//AC3 Testing
+	for (int i = 0; i < 3; i++)
+	{
+		system("cls");
+		cout << "Testing the AC3 function...\n";
+		getSudokuCSP(puzzles[i], assignment, domain);
+		getSudokuBoard(table);
+		fillDisplayBoard(table, assignment);
+		cout << "The puzzle:\n";
+		displayBoard(table);
+		cout << endl << "The CSP domain:\n";
+		displaySudokuCSP(assignment, domain);
+		AC3(domain, C);
+		cout << endl << "The CSP domain after AC3:\n";
+		displaySudokuCSP(assignment, domain);
+		system("pause");
+
+	}
+
+
+	//MRV testing
+
+	for (int i = 0; i < 3; i++)
+	{
+		system("cls");
+		cout << "Testing the backTracking with the MRV heuristic function...\n";
+		getSudokuCSP(puzzles[i], assignment, domain);
+		getSudokuBoard(table);
+		fillDisplayBoard(table, assignment);
+		cout << "The puzzle:\n";
+		displayBoard(table);
+		cout << endl << "The solution:\n";
+		int count = 0;
+		for (int i = 0; i < 81; i++)
+			if (assignment[i] > 0)
+				count++;
+		vector<int> result = backtrackCSP_MRV(assignment, count, domain, C);
+		fillDisplayBoard(table, result);
+		displayBoard(table);
+		system("pause");
+
+	}
+
+
+	system("pause");
+	return 0;
+}
+
+////////Drawing functions////////////////////////////
+
+void getSudokuBoard(string table[])
+{
+	string table1[] = {
+	"+===========+===========+===========+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+===========+===========+===========+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+===========+===========+===========+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"+---*---*---+---*---*---+---*---*---+",
+	"] 0 | 0 | 0 ] 0 | 0 | 0 ] 0 | 0 | 0 ]",
+	"=====================================", };
+
+	for (int i = 0; i < 19; i++)
+	{
+		table[i] = table1[i];
+		for (int j = 0; j < table[i].length(); j++)
+		{
+			switch (table[i][j])
+			{
+			case '+': table[i][j] = char(219); break;
+			case ']': table[i][j] = char(219); break;
+			case '=': table[i][j] = char(219); break;
+			case '-': table[i][j] = char(196); break;
+			case '|': table[i][j] = char(179); break;
+			case '*': table[i][j] = char(197); break;
+			case '0': table[i][j] = ' '; break;
+			}
+
+		}
+	}
+}
+
+void displayBoard(string table[])
+{
+	for (int i = 0; i < 19; i++)
+	{
+		cout << table[i] << endl;
+	}
+}
+
+void fillDisplayBoard(string board[], int sudokuM[][9])
+{
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+		{
+			if (sudokuM[i][j] > 0)
+				board[1 + 2 * i][2 + 4 * j] = char(sudokuM[i][j] + '0');
+		}
+}
+
+void fillDisplayBoard(string board[], vector<int> assignment)
+{
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+		{
+			if (assignment[i * 9 + j] > 0)
+				board[1 + 2 * i][2 + 4 * j] = char(assignment[i * 9 + j] + '0');
+		}
+}
+
+
+void displayBoard(string table[], int T[][9])
+{
+	//https://en.wikipedia.org/wiki/ANSI_escape_code
+	char blue[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
+	char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
+	for (int i = 0; i < 19; i++)
+	{
+		for (int j = 0; j < table[i].length(); j++)
+		{
+			if (i < 18 && T[i / 2][j / 4]>0 && j % 4 == 2 && i % 2 == 1)
+				cout << blue << table[i][j] << normal;
+			else
+				cout << table[i][j];
+
+		}
+		cout << endl;
+	}
+}
+
+void displaySudokuCSP(vector<int> assignment, vector<int> domain[81])
+{
+	string s1 = " . . .  . . .  . . .    . . .  . . .  . . .    . . .  . . .  . . .";
+	string s2 = "                                                                  ";
+	string board[] = { s1,s1,s1,s2,s1,s1,s1,s2,s1,s1,s1,s2,s2, s1,s1,s1,s2,s1,s1,s1,s2,s1,s1,s1,s2,s2, s1,s1,s1,s2,s1,s1,s1,s2,s1,s1,s1, };
+	for (int a = 0; a < 81; a++)
+	{
+		int i = a / 9, j = a % 9;
+		if (assignment[a] > 0.5)
+		{
+			int v = assignment[a] - 1;
+			int r1 = v / 3, c1 = v % 3;
+			int x = i * 4 + r1 + i / 3;
+			int y = j * 7 + c1 * 2 + j / 3 * 2 + 1;
+			board[x][y] = char(v + '1');
+
+		}
+		else
+		{
+			for (int k = 0; k < domain[a].size(); k++)
+			{
+				int v = domain[a][k] - 1;
+				int r1 = v / 3, c1 = v % 3;
+				int x = i * 4 + r1 + i / 3;
+				int y = j * 7 + c1 * 2 + j / 3 * 2 + 1;
+				board[x][y] = char(v + '1');
+
+			}
+		}
+	}
+	for (int i = 0; i < 37; i++)
+		cout << board[i] << endl;
+}
+
+void displaySudokuSimple(vector<int> assignment)
+{
+	string s1 = " . . .  . . .  . . .";
+	string s2 = "                    ";
+	string board[] = { s1,s1,s1,s2,s1,s1,s1,s2,s1,s1,s1 };
+	for (int a = 0; a < 81; a++)
+	{
+		int i = a / 9, j = a % 9;
+		int x = i + i / 3;
+		int y = j * 2 + j / 3 + 1;
+
+		if (assignment[a] > 0.5)
+		{
+			int v = assignment[a];
+			board[x][y] = char(v + '0');
+		}
+	}
+	for (int i = 0; i < 11; i++)
+		cout << board[i] << endl;
+}
+
+//////////////////////////////////////////////////////
+
+/////Helper Functions///////////////////////////////////
+void fillConst(bool C[81][81])
+{
+	for (int i = 0; i < 81; i++)
+		for (int j = 0; j < 81; j++)
+			C[i][j] = false;
+
+	for (int a = 0; a < 9; a++)
+	{
+		for (int b = 0; b < 9; b++)
+		{
+			int z = 9 * a + b;
+			for (int i = 0; i < 9; i++)
+			{
+				int z1 = 9 * i + b;  //same column
+				int z2 = 9 * a + i;  //same row
+				if (z != z1)
+					C[z][z1] = true;
+				if (z != z2)
+					C[z][z2] = true;
+			}
+			int r = a / 3, c = b / 3;
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+				{
+					int z3 = 9 * (3 * r + i) + (3 * c + j);
+					if (z != z3)
+						C[z][z3] = true;  //same cell
+
+				}
+		}
+
+	}
+}
+
+void getSudokuCSP(string puzzle, vector<int>& assignment, vector<int> domain[81])
+{
+	vector<int>  d = { 1,2,3,4,5,6,7,8,9 };
+	vector<int> assignment2(81, 0);
+
+	for (int a = 0; a < 81; a++)
+	{
+		int v = int(puzzle[a] - '0');
+		assignment2[a] = v;
+		domain[a].clear();
+		if (v > 0)
+			domain[a].push_back(v);
+		else
+			domain[a] = d;
+	}
+	assignment = assignment2;
+}
+
+////////////////////////////////////////////////////////
+
+
+//Base bactrack implementation
+vector<int> backtrackCSP(vector<int> assignment, int count, vector<int> domain[81], const bool C[81][81])
+{
+	vector<int> result;
+	if (count >= 81) //if assignment complete
+		return assignment;
+	//pick the next unassigned variable
+	int r = 0;
+	while (assignment[r] > 0)
+		r++;
+	int n = domain[r].size();
+	for (int i = 0; i < n; i++)
+	{
+		int value = domain[r][i];  //pick the next value
+
+		//check for if value is consitent with the past
+		bool consistent = true;
+		for (int j = 0; j < 81; j++)
+			if (j != r && C[r][j] && assignment[j] == value)
+			{
+				consistent = false;
+				break;
+			}
+		if (consistent)
+		{
+			assignment[r] = value;
+			result = backtrackCSP(assignment, count + 1, domain, C);
+			if (result.size() > 0)
+				return result;
+			assignment[r] = 0;
+		}
+	}
+	return result;
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//To be implemented by the students
+
+bool AC3(vector<int> domain[81], const bool C[81][81])
+{
+	return true;
+}
+
+bool revise(int xi, int xj, vector<int> domain[81], const bool C[81][81])
+{
+	return true;
+}
+
+
+vector<int> backtrackCSP_MRV(vector<int> assignment, int count, vector<int> domain[81], const bool C[81][81])
+{
+	vector<int> result;
+	return result;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
